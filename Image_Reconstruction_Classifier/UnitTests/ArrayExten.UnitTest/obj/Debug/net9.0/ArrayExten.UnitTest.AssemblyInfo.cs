df6c6ecc3a8072ff58165f4d8ed63469db174d67
Eyad <Eyad_Elsheita@outlook.com>
2026-06-24 23:04:58 +0200

    fixing the Error with the Sixlabor library

--- Image_Reconstruction_Classifier/UnitTests/ArrayExten.UnitTest/obj/Debug/net9.0/ArrayExten.UnitTest.AssemblyInfo.cs
+++ Image_Reconstruction_Classifier/UnitTests/ArrayExten.UnitTest/obj/Debug/net9.0/ArrayExten.UnitTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArrayExten.UnitTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4112d817cc9fdd5bfe8feb42942d5036eaa956b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+143d5e0eef5c0a581782215903ebca33ac0f613b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArrayExten.UnitTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArrayExten.UnitTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Image_Reconstruction_Classifier/UnitTests/HTM.UnitTest/obj/Debug/net9.0/HTM.UnitTest.AssemblyInfo.cs
+++ Image_Reconstruction_Classifier/UnitTests/HTM.UnitTest/obj/Debug/net9.0/HTM.UnitTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HTM.UnitTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4112d817cc9fdd5bfe8feb42942d5036eaa956b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+143d5e0eef5c0a581782215903ebca33ac0f613b")]
 [assembly: System.Reflection.AssemblyProductAttribute("HTM.UnitTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HTM.UnitTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
