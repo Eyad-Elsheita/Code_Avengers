ed1279ed40661b1c878d5e5a4d4d80b7fa921092
Eyad <Eyad_Elsheita@outlook.com>
2026-07-08 19:17:55 +0200

    Fixing Sixlabors Problem and Solving all the Build up error s

--- Image_Reconstruction_Classifier/UnitTests/ImageSpatial.UnitTest/obj/Debug/net9.0/ImageSpatial.UnitTest.AssemblyInfo.cs
+++ Image_Reconstruction_Classifier/UnitTests/ImageSpatial.UnitTest/obj/Debug/net9.0/ImageSpatial.UnitTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ImageSpatial.UnitTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+945f5c06a475ecce9319fc29b8a7aaeda49f5f2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8f7b6d35ad8b74ae66ba8e15eb75fe03d2d449a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ImageSpatial.UnitTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ImageSpatial.UnitTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
